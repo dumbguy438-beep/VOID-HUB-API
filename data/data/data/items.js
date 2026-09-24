17fdd3f1eaafe434bc31174f0e1bc2a596f03c61
dumbguy438-beep <dumbguy438@gmail.com>
2026-09-24 17:56:21 +0530

    Create items.js

---
+++ data/data/data/items.js
@@ -0,0 +1,34 @@
+module.exports = {
+    legendary: [
+        { name: "God's Chalice",         type: "Quest Item",  desc: "Used to summon rip_indra True Form" },
+        { name: "Sweet Chalice",         type: "Quest Item",  desc: "Used to summon Dough King" },
+        { name: "Fist of Darkness",      type: "Quest Item",  desc: "Used to summon Darkbeard" },
+        { name: "Hallow Essence",        type: "Quest Item",  desc: "Used to enter Hell Dimension" },
+        { name: "Red Key",               type: "Key",         desc: "Opens Red Door in Cake Land" },
+        { name: "Library Key",           type: "Key",         desc: "Opens Ice Castle Library" },
+        { name: "Water Key",             type: "Key",         desc: "Unlocks Sharkman Karate" },
+        { name: "Hidden Key",            type: "Key",         desc: "Found in Snow Mountain" },
+        { name: "Dragon Egg",            type: "Quest Item",  desc: "Used for Draco race" },
+        { name: "Tushita",               type: "Sword",       desc: "Legendary sword" },
+        { name: "Cursed Dual Katana",    type: "Sword",       desc: "Legendary dual katana" }
+    ],
+    materials: [
+        { name: "Leather",               type: "Material" },
+        { name: "Scrap Metal",           type: "Material" },
+        { name: "Angel Wings",           type: "Material" },
+        { name: "Magma Ore",             type: "Material" },
+        { name: "Fish Tail",             type: "Material" },
+        { name: "Radioactive Material",  type: "Material" },
+        { name: "Ectoplasm",             type: "Material" },
+        { name: "Mystic Droplet",        type: "Material" },
+        { name: "Vampire Fang",          type: "Material" },
+        { name: "Demonic Wisp",          type: "Material" },
+        { name: "Conjured Cocoa",        type: "Material" },
+        { name: "Dragon Scale",          type: "Material" },
+        { name: "Gunpowder",             type: "Material" },
+        { name: "Mini Tusk",             type: "Material" },
+        { name: "Bones",                 type: "Material" },
+        { name: "Dark Fragment",         type: "Material" },
+        { name: "Leviathan Heart",       type: "Material" }
+    ]
+};
