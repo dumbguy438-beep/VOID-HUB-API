a4f0b5ffdd3fe4a57d89bba2d6a9b5837620ce65
dumbguy438-beep <dumbguy438@gmail.com>
2026-09-24 17:53:17 +0530

    Create islands.js

---
+++ data/data/islands.js
@@ -0,0 +1,47 @@
+module.exports = {
+    1: {
+        "Pirate Starter":    { cframe: [-1087, 46, 4040],    region: "Starter Island" },
+        "Marine Starter":    { cframe: [-2848, 7, 5356],     region: "Starter Island" },
+        "Jungle":            { cframe: [-1600, 36, 150],     region: "Jungle" },
+        "Pirate Village":    { cframe: [-1153, 57, 3820],    region: "Pirate Village" },
+        "Desert":            { cframe: [888, 15, 4394],      region: "Desert" },
+        "Frozen Village":    { cframe: [1382, 87, -1302],    region: "Frozen Village" },
+        "Marine Fortress":   { cframe: [-5033, 35, 4329],    region: "Marine Fortress" },
+        "Skylands":          { cframe: [-4846, 763, -2618],  region: "Skylands" },
+        "Prison":            { cframe: [5306, 2, 474],       region: "Prison" },
+        "Colosseum":         { cframe: [-1580, 7, -2979],    region: "Colosseum" },
+        "Magma Village":     { cframe: [-5320, 9, 8516],     region: "Magma Village" },
+        "Underwater City":   { cframe: [61160, 5, 1819],     region: "Underwater" },
+        "Upper Skylands":    { cframe: [-7890, 5643, -1405], region: "Skylands" },
+        "Fountain City":     { cframe: [5258, 39, 4051],     region: "Fountain City" }
+    },
+    2: {
+        "Kingdom of Rose":   { cframe: [-422, 73, 1841],     region: "Kingdom of Rose" },
+        "Green Zone":        { cframe: [-2446, 73, -3221],   region: "Green Zone" },
+        "Graveyard":         { cframe: [-5489, 49, -797],    region: "Graveyard" },
+        "Snow Mountain":     { cframe: [602, 401, -5367],    region: "Snow Mountain" },
+        "Hot and Cold":      { cframe: [-6227, 82, -4852],   region: "Hot and Cold" },
+        "Cursed Ship":       { cframe: [923, 126, 32852],    region: "Cursed Ship" },
+        "Ice Castle":        { cframe: [5670, 28, -6484],    region: "Ice Castle" },
+        "Forgotten Island":  { cframe: [-3053, 240, -10147], region: "Forgotten Island" },
+        "Dressrosa":         { cframe: [-347, 73, 297],      region: "Dressrosa" },
+        "Swan Mansion":      { cframe: [2285, 15, 905],      region: "Swan Mansion" },
+        "Zombie Island":     { cframe: [-6509, 83, -133],    region: "Zombie Island" },
+        "Fountain City":     { cframe: [5258, 39, 4051],     region: "Fountain City" }
+    },
+    3: {
+        "Port Town":         { cframe: [-451, 109, 5949],    region: "Port Town" },
+        "Hydra Island":      { cframe: [5212, 1004, 756],    region: "Hydra Island" },
+        "Great Tree":        { cframe: [2682, 1682, -7190],  region: "Great Tree" },
+        "Floating Turtle":   { cframe: [-13230, 333, -7632], region: "Floating Turtle" },
+        "Haunted Castle":    { cframe: [-9486, 143, 5567],   region: "Haunted Castle" },
+        "Sea of Treats":     { cframe: [-1165, 60, -14488],  region: "Sea of Treats" },
+        "Tiki Outpost":      { cframe: [-16546, 56, -168],   region: "Tiki Outpost" },
+        "Castle on the Sea": { cframe: [-5036, 315, -3179],  region: "Castle on the Sea" },
+        "Mansion":           { cframe: [-12462, 375, -7552], region: "Mansion" },
+        "Temple of Time":    { cframe: [28286, 14897, 103],  region: "Temple of Time" },
+        "Cake Land":         { cframe: [-2015, 38, -12031],  region: "Cake Land" },
+        "Candy Village":     { cframe: [-1165, 60, -14488],  region: "Candy Village" },
+        "Submerged Island":  { cframe: [-3200, -10, -10000], region: "Submerged Island" }
+    }
+};
